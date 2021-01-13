21be36913bab9946435f3a6c26577df32b18e77f
Elis Kostromina <liza.kostr.2005@gmail.com>
2021-01-13 22:51:05 +0700

    new circles

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 import sys
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5.QtCore import QRect, QMetaObject, QCoreApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton,\
+    QWidget, QMenuBar, QStatusBar
 from random import randrange
 
 
-class Example(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(452, 330)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(150, 220, 161, 31))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 452, 26))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u041d\u0410\u0416\u041c\u0418 \u041d\u0410 \u041c\u0415\u041d\u042f", None))
+    # retranslateUi
+
+
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
@@ -27,9 +58,9 @@ class Example(QMainWindow):
     def draw_circles(self, qp):
         a = randrange(5, 100)
         b = randrange(5, 100)
-        qp.setBrush(QColor(255, 220, 51))
+        qp.setBrush(QColor(*(randrange(256) for _ in range(3))))
         qp.drawEllipse(50, 50, a, a)
-        qp.setBrush(QColor(255, 220, 51))
+        qp.setBrush(QColor(*(randrange(256) for _ in range(3))))
         qp.drawEllipse(270, 70, b, b)
 
 
